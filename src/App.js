fe72c6daf1c3d252970b2515a0a25c8f8fca2cdb
Karera-o <olivierkarera2020@gmail.com>
2022-09-06 12:30:42 +0200

    adding hashrouter

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Create from './ui/pages/signinPage/Create';
 import Signinl from './ui/pages/signinPage/Signinl';
 import HomePage from './ui/pages/homePage/homePage';
 // import SigninPage from './ui/pages/signinPage/signinPage';
-import {BrowserRouter  , Routes ,Route} from 'react-router-dom';
+import {HashRouter as Router  , Routes ,Route} from 'react-router-dom';
 import Container from './ui/pages/container/container.jsx'
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
+    <Router>
       <Routes>
     
 
@@ -25,7 +25,7 @@ function App() {
      <Route element={<Container/>} path="/dash" />
     
     </Routes>
-    </BrowserRouter>
+    </Router>
     </div>
   );
 }
